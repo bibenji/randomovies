f917603af623a226d6596513e4fd7fac01a32c23
Benjamin Billette <benjamin.billette@ikimea.com>
2018-01-03 01:00:25 +0100

    modif Procfile for deploy

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,10 @@ class AppKernel extends Kernel
             new JMS\SerializerBundle\JMSSerializerBundle(),
         ];
 
+        if(in_array($this->getEnvironment(), array('prod'))) {
+            $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
+        }
+
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
