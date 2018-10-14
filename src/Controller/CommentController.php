efc1edf8b39b1673c3966d30ae283634190a1ae1
bibenji <benjamin.billette@hotmail.fr>
2018-10-14 19:18:43 +0200

    point to comment after comment is saved

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -7,6 +7,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 
 class CommentController extends Controller
 {
@@ -33,9 +34,10 @@ class CommentController extends Controller
     {
         if ($this->getUser() !== $comment->getUser()) {
             throw new UnauthorizedHttpException('Vous n\'êtes pas autorisé à faire ça !');
-        }
+        }        
 
-        $commentForm = $this->createForm('Randomovies\Form\CommentType', $comment);
+        $commentForm = $this->createForm('Randomovies\Form\CommentType', $comment);        
+        
         $commentForm->handleRequest($request);
 
         if ($commentForm->isSubmitted() && $commentForm->isValid()) {
@@ -44,11 +46,26 @@ class CommentController extends Controller
             $em = $this->getDoctrine()->getManager();
             $em->persist($comment);
             $em->flush($comment);
-
-            return $this->redirectToRoute('user-account');
+            
+            $redirect = $commentForm->get('referer')->getData();
+            $redirect = preg_match('/#commentId/', $redirect) ? $redirect : $redirect.'#commentId'.$comment->getId();            
+            	
+            return $this->redirect($redirect);
         }
-
+        
+        $referer = $request->headers->get('referer');
+                
+        if ($referer !== $request->getUri()) {
+        	$commentForm = $this->createForm('Randomovies\Form\CommentType', $comment, [
+        		'referer' => $referer,		
+        	]);
+        }
+        
+        $backPath = $commentForm->get('referer')->getData();
+        $backPath = preg_match('/#commentId/', $backPath) ? $backPath : $backPath.'#commentId'.$comment->getId();
+		
         return $this->render('comment/edit.html.twig', [
+        	'back_path' => $backPath,
             'comment' => $comment,
             'comment_form' => $commentForm->createView()
         ]);
